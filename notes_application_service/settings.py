582eccc549cf012f8c6757433dec3cdd86e8fc5b
maxitoska <maxitosm00@gmail.com>
2024-05-08 01:56:52 +0300

    changed filters styles, added localhost to allowed hosts

--- notes_application_service/settings.py
+++ notes_application_service/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["notes-application-qlmn.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "notes-application-qlmn.onrender.com"]
 
 # Application definition
 
