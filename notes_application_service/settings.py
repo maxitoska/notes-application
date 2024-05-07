9ea1db1a0c94fdeb0cc2f73581e50bdbf50d6414
maxitoska <maxitosm00@gmail.com>
2024-05-08 01:00:08 +0300

    added static root to settings.py

--- notes_application_service/settings.py
+++ notes_application_service/settings.py
@@ -116,7 +116,7 @@ LOGIN_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
