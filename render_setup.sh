6ba1edafbc57b5d978152c4a60241327e5ed5731
maxitoska <maxitosm00@gmail.com>
2024-05-08 01:43:12 +0300

    changes in render_setup.sh

--- render_setup.sh
+++ render_setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
 
-# Install Python dependencies
+
 pip install -r requirements.txt
 
-# Run Django database migrations
+python manage.py makemigrations
+
 python manage.py migrate
 
-# Collect static files
 python manage.py collectstatic --noinput
 
-# Start the Django server
 gunicorn notes_application_service.wsgi:application --bind 0.0.0.0:$PORT
